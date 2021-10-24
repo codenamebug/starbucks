215f62b054a73869a15bb506aa09c10a1baa2491
AbyulStudy <kinsng1599@gmail.com>
2021-10-24 13:05:53 +0900

    :sparkles:Added react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
 import './App.css';
 import Header from './Component/Header';
-
+import {Route, Switch} from "react-router-dom"
+import Drink from './Pages/Drink'
+import Card from './Pages/Card'
+import Merchandise from './Pages/Merchandise'
+import Food from './Pages/Food'
 function App() {
   return (
-    <div id="top__wrap">
+    <div id="wrap">
       <Header />
+        <Switch>
+          <Route exact path="/">
+            
+          </Route>
+          <Route exact path="/drink">
+            <Drink />
+          </Route>
+          <Route exact path="/card">
+            <Card />
+          </Route>
+          <Route exact path="/merchandise">
+            <Merchandise></Merchandise>
+          </Route>
+          <Route exact path="/food">
+            <Food></Food>
+          </Route>
+        </Switch>
     </div>
   );
 }
